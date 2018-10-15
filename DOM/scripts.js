d8321fb8af094b915db843c32b768ad86c9acdf7
annmcalvert <annmcalvert@gmail.com>
2018-10-15 10:53:36 -0500

    done with DOM'

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -1,42 +1,59 @@
-let div = document.createElement('div');
-let btn = document.createElement('button');
+let div1 = document.getElementById('div1');
+let btn1 = document.createElement('button');
 let btnText = document.createTextNode('Click Me');
-let h3 = document.createElement('h3');
-let h3Text = document.createTextNode('1.');
+let btn2 = document.getElementById('btn2');
+let div3 = document.getElementById('div3');
+let para = document.getElementById('para');
+let btn5 = document.getElementById('btn5');
+let nameP = document.createElement('p');
+let name = document.createTextNode('Ann Calvert');
+let nameDiv = document.getElementById('nameDiv');
+let friends = ['Dennis', 'Steve', 'Everett', 'Anna', 'Ashley', 'Melissa', 'Pracilla', 'Rainey', 'Polly', 'Ripley'];
+let btn6 = document.getElementById('btn6');
+let list = document.getElementById('list');
 
-h3.appendChild(h3Text);
-div.appendChild(h3);
-btn.appendChild(btnText);
-div.appendChild(btn);
-document.body.appendChild(div);
+btn1.appendChild(btnText);
+div1.appendChild(btn1);
+nameP.appendChild(name);
 
 //1. button with nice message
-btn.addEventListener('click', function () {
+btn1.addEventListener('click', function () {
     alert('Have a nice day!');
 });
 
+//2. alert message in input box
+btn2.addEventListener('click', function () {
+    let text = document.getElementById('text').value;
+    alert(text);
+});
+
 //3. div hover color
-let divSquare = document.getElementById('div');
-divSquare.addEventListener('mouseover', function() {
+div3.addEventListener('mouseover', function () {
     (this).style.backgroundColor = 'blue';
 });
-divSquare.addEventListener('mouseleave', function() {
+div3.addEventListener('mouseleave', function () {
     (this).style.backgroundColor = 'black';
 });
 
 //4. <p> that will change to a random color
-let para = document.getElementById('para');
-para.addEventListener('click', function() {
+para.addEventListener('click', function () {
     let randomColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
     (this).style.color = randomColor;
 });
 
 //5. button that shows name
-let nameBtn = document.getElementById('nameBtn');
-let nameP = document.createElement('p');
-let name = document.createTextNode('Ann Calvert');
-nameP.appendChild(name);
-let nameDiv = document.getElementById('nameDiv');
-nameBtn.addEventListener('click', function() {
+btn5.addEventListener('click', function () {
     nameDiv.appendChild(nameP);
-})
+});
+
+//6. button that adds friends to list
+btn6.addEventListener('click', function () {
+    friends.forEach(listEach);
+    function listEach(friends) {
+        let li = document.createElement('li');
+        let liText = document.createTextNode(friends);
+        li.appendChild(liText);
+        document.body.appendChild(li);
+    };
+});
+
